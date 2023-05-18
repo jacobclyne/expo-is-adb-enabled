782af0e4bc81232e1c7b260fa8b2a1c2da8b9fcb
Hodeem Miller <hodeem@Hodeems-MacBook-Pro.local>
2023-05-18 14:23:21 -0500

    Added platform check before exporting isAdbEnabled

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 import ExpoIsAdbEnabledModule from "./ExpoIsAdbEnabledModule";
+import { Platform } from "react-native";
 
-export const isAdbEnabled: boolean = ExpoIsAdbEnabledModule.isAdbEnabled();
+export const isAdbEnabled: boolean = Platform.OS == "android" ? ExpoIsAdbEnabledModule.isAdbEnabled() : false ;
