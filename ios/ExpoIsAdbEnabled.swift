8494a3814a7a61cdb37af9ebf713254c9c1e904f
Hodeem Miller <hodeem@Hodeems-MacBook-Pro.local>
2023-05-27 09:06:24 -0500

    Removed ios and web from supported platforms in expo-module.config.json

--- ios/ExpoIsAdbEnabled.swift
+++ ios/ExpoIsAdbEnabled.swift
@@ -1,6 +1,6 @@
 import ExpoModulesCore
 
-public class ExpoIsAdbEnabledModule: Module {
+public class ExpoIsAdbEnabled: Module {
   // Each module class must implement the definition function. The definition consists of components
   // that describes the module's functionality and behavior.
   // See https://docs.expo.dev/modules/module-api for more details about available components.
